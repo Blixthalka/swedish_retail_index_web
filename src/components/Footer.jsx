78cc87456f4c073fe9315556d9aac8d94413eda3
Emil Nilsson <emil@sharpfin.com>
2023-01-15 18:06:33 +0100

    empty

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,6 @@
 import { CodeBracketIcon } from '@heroicons/react/24/outline';
 
-function Footer({ }) {
+function Footer() {
     return (
         <div className='flex items-center place-content-center'>
             <span className="text-center  text-white text-sm">Crafted by <a href="https://twitter.com/blixthalka" className="text-blue-400 hover:text-blue-500">@blixthalka</a></span>
