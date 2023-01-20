edc5abd7a64b507fe42257f7327d87acfc08502f
Emil Nilsson <emil@sharpfin.com>
2023-01-20 10:14:48 +0100

    add numbering on table

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,11 +3,9 @@ import { ArrowPathIcon } from '@heroicons/react/24/outline';
 function Footer() {
     return (
         <div>
-
             <div className='flex items-center  mt-5'>
                 <ArrowPathIcon className="w-5 h-5 text-gray-500 mr-3" />
-                <span className="text-center  text-white text-sm">Follow <a href="https://twitter.com/srindex_" className="text-blue-400 hover:text-blue-500">@srindex_</a> for Twitter updates.</span>
-
+                <span className="text-center  text-white text-sm"><a href="https://twitter.com/srindex_" className="text-blue-400 hover:text-blue-500">@srindex_</a> on Twitter.</span>
             </div>
         </div>
     )
--- src/components/IncludedStocksTable.jsx
+++ src/components/IncludedStocksTable.jsx
@@ -1,5 +1,4 @@
-import React from 'react';
-import { useState, useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import Button from './Button';
 
 const IncludedStocksTable = ({ className }) => {
@@ -23,14 +22,16 @@ const IncludedStocksTable = ({ className }) => {
             <table className="">
                 <thead>
                     <tr>
+                        <th className="th-num hidden sm:table-cell">#</th>
                         <th className="">Name</th>
                         <th className="th-num">Owners</th>
                         <th className="th-num">Weight</th>
                     </tr>
                 </thead>
                 <tbody>
-                    {instruments.map((row) => (
+                    {instruments.map((row, i) => (
                         <tr>
+                            <td className="td-num hidden sm:table-cell w-0">{i + 1}</td>
                             <td>{row.name}</td>
                             <td className="td-num">{new Intl.NumberFormat("en-GB", {minimumFractionDigits: 0}).format(row.owners)}</td>
                             <td className="td-num">{`${new Intl.NumberFormat("se-SE", {minimumFractionDigits: 2}).format(row.weight)} %`}</td>
