3bf15d87cc8dceb283adf2e81788d306ff2588d1
Emil Nilsson <emil@sharpfin.com>
2023-01-28 15:33:39 +0100

    add the chart also

---
+++ src/components/BarChart.jsx
@@ -0,0 +1,81 @@
+import ReactECharts from 'echarts-for-react';
+import React from 'react';
+import { formatShortDate } from '../utils/util';
+
+
+
+
+function Chart({ data, ...other }) {
+
+    let option = {
+        backgroundColor: "transparent",
+        textStyle: {
+            color: '#9ca3af',
+            fontFamily: 'JetBrains Mono, sans-serif',
+        },
+        legend: {
+            show: false,
+        },
+        xAxis: {
+            type: "category",
+            axisLabel: {
+                fontSize: 12,
+                formatter: formatShortDate
+            }
+        },
+        yAxis: {
+            type: 'value',
+            position: 'right',
+            splitLine: {
+                lineStyle: {
+                    type: "dashed",
+                    color: "#1f2937"
+                }
+            },
+            axisLabel: {
+                show: false,
+                inside: false,
+                showMinLabel: false,
+                fontSize: 12
+            }
+        },
+        tooltip: {
+            trigger: 'axis',
+            borderColor: '#1f2937',
+            backgroundColor: "#101011",
+            textStyle: {
+                fontSize: 14,
+                color: "#fff"
+            },
+         },
+        grid: {
+            left: '5px',
+            right: '5px',
+            bottom: '0',
+            top: '5px',
+            containLabel: true
+        },
+        series: [
+            {
+                data: data.map(d => [d.date, d.owners]),
+                type: 'bar',
+                color: '#2563eb',
+                name: "Ägare",
+            }
+        ]
+    };
+
+
+    return (
+        <div {...other} style={{marginRight: '-5px', marginLeft: '-5px'}}>
+            <ReactECharts
+                option={option}
+                notMerge={true}
+                lazyUpdate={true}
+            />
+        </div>
+
+    );
+}
+
+export default Chart;
